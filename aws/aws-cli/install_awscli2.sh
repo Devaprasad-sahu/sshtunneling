0f8533fbaca0af6881137a0a273d7931e6f14416
ANIL <anil@ncodeit.com>
2021-04-01 19:16:43 +0530

    syntax error fixed

--- aws/aws-cli/install_awscli2.sh
+++ aws/aws-cli/install_awscli2.sh
@@ -4,4 +4,4 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install --bin-dir /usr/bin --install-dir /usr/bin/aws-cli --update
 aws --version
-echo "run 'aws configure' to configure the aws cli credentials
+echo "run 'aws configure' to configure the aws cli credentials"
